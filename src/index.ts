60f5ea98aed16c1f5464ee079fa1cc8d2ff20d05
Chopper <riskidrian1@gmail.com>
2024-11-30 12:04:22 +0700

    feat: route GET, POST

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+class Note {
+  constructor(public data: string[] = ["Moonhalo"]) {}
+}
+
+const app = new Elysia()
+  .use(swagger())
+  .decorate("note", new Note())
+  .get("/note", ({ note }) => note.data)
+  .get("/", () => "Hello Elysia")
+  .post("/hello", "Do you miss me?")
+  .get("/context", ({ path }) => path)
+  .get("/user/:id", ({ params: { id } }) => id, {
+    params: t.Object({
+      id: t.Numeric(),
+    }),
+  })
+  .post("/form", ({ body }) => body)
+  .listen(3000);
+
+export type App = typeof app;
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
